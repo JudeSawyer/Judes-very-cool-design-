ee1ee5d09c257447e63d659a2f1c1e8cb4d6f1a8
Jude Sawyer <Judes@thedeltaschool.org>
2019-12-26 16:43:52 -0600

    Adding my playground file to GitHub

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var CoolBeans = "Hello, playground"
+
+var integer = 10
+var doubleTester: Double = 10.053242
+
+var string2 = "strings always exist in quotes"
+
+var integer2 = 20
+
+CoolBeans
+
+integer
+integer2
+
+
+integer + integer2
+integer * integer2
+integer2 / integer
+
+CoolBeans + string2
+CoolBeans + String(integer)
